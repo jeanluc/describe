6822ba33a1130c945e6e82df5ebb2b27a53310c8
Jean-Luc Barras <jeanluc.barras@gmail.com>
2011-01-17 15:19:56 +0100

    Done with static pages and tests on title

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Describe::Application.routes.draw do
   get "pages/help"
 
   get "pages/contact"
+  
+  get "pages/about"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector('title',
+          :content => 'Describe | Home')
     end
   end
 
@@ -14,6 +20,11 @@ describe PagesController do
       get 'help'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector('title',
+          :content => 'Describe | Help')
+    end
   end
 
   describe "GET 'contact'" do
@@ -21,6 +32,23 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector('title',
+          :content => 'Describe | Contact')
+    end
   end
+  
+  describe "GET 'about'" do
+     it "should be successful" do
+       get 'about'
+       response.should be_success
+     end
+     it "should have the right title" do
+       get 'about'
+       response.should have_selector('title',
+           :content => 'Describe | About')
+     end
+   end
 
 end
