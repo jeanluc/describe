fd94cb0463eea9b19762e83f560721ca96aec80f
Jean-Luc Barras <jeanluc.barras@gmail.com>
2011-01-17 17:19:43 +0100

    Created User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Describe::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
 
   get "pages/help"
@@ -63,6 +65,8 @@ Describe::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
+  
+  match '/signup',  :to => 'users#new'
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
